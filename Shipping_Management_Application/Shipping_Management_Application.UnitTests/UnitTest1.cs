e22f7b1c25ad9b502c673f0f47de023d5df2aed9
mbJobb <71829138+mbJobb@users.noreply.github.com>
2023-10-26 17:17:23 +0200

    created folder structure

---
+++ Shipping_Management_Application/Shipping_Management_Application.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Shipping_Management_Application.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
