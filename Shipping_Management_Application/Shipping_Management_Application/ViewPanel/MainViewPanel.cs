fecc9d558ab2cd910b679bcb8bc76dc2fe51c7aa
saro1993 <116103507+saro1993@users.noreply.github.com>
2023-11-12 14:21:25 +0100

    created MainViewPanel

--- Shipping_Management_Application/Shipping_Management_Application/ViewPanel/MainViewPanel.cs
+++ Shipping_Management_Application/Shipping_Management_Application/ViewPanel/MainViewPanel.cs
@@ -1,13 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
-namespace Shipping_Management_Application.ViewPanel
+public class MainViewPanel
 {
-    public class MainViewPanel
+    public void MainView()
     {
-        // Login view
+        Console.WriteLine("Welcome to Our Shipping Management. We are here to help you take care of your orders!");
+        Thread.Sleep(3000);
+        Console.WriteLine("1. LogIn for Customer");
+        Console.WriteLine("2. LogIn for Admin");
+        Console.WriteLine("3. Exit");
+
+        var userInput = Console.ReadLine();
+        switch (userInput)
+        {
+            case "1":
+                Console.WriteLine("LogIn for Customer");// comming method 
+                Console.WriteLine("Loading....");
+                Thread.Sleep(3000);
+                break;
+            case "2":
+                Console.WriteLine("LogIn for Admin"); // comming method 
+                Console.WriteLine("Loading....");
+                Thread.Sleep(3000);
+                break;
+            case "3":
+                Console.WriteLine("Exiting");
+                break;
+
+            default:
+                Console.WriteLine("Invalid option. Please enter 1, 2 or 3.");
+                break;
+        }
+
+      
+    }
+    //Check out is input isValid 
+    public bool IsValidInput(string input)
+    {
+        return input == "1" || input == "2" || input == "3";
     }
 }
