aaf626ea9f151479b77c7ed55add244fc9127ef6
Henrik-Pham <HenrikPHPham@gmail.com>
2023-11-15 22:29:18 +0100

    Making log in work

--- Shipping_Management_Application/Shipping_Management_Application/Data/CrudOperations.cs
+++ Shipping_Management_Application/Shipping_Management_Application/Data/CrudOperations.cs
@@ -5,20 +5,17 @@ namespace Shipping_Management_Application.Data
 {
     public static class CrudOperations
     {
-        public static User GetUserByUserNmaeAndPassword(string? userName, string? password)
-        {
+        public static User? GetUserByUserNameAndPassword(string? userName, string? password){
+
+            using DataContext context = new DataContext();
+
+            if (string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(password)){
+                throw new ArgumentNullException("userName");
+            }
             
-            using  DataContext context = new ();
-
-            if(userName is null || password is null)
-            {
-                return ArgumentNullException;
-
-            } 
             User? user = context.Users.FirstOrDefault(u => u.UserName == userName && u.Password == password);
+            Console.WriteLine($"Welcome {user?.UserName}");
             return user;
-
         }
-
     }
 }
