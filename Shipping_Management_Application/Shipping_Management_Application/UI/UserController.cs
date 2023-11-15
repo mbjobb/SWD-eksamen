cca01444774ef2d68f7bcd050adb2bea82eb6f99
mbJobb <71829138+mbJobb@users.noreply.github.com>
2023-11-15 22:54:23 +0100

    restored Henriks version, the caveman way

---
+++ Shipping_Management_Application/Shipping_Management_Application/UI/UserController.cs
@@ -0,0 +1,43 @@
+﻿using Shipping_Management_Application.BuisnessLogic.User;
+using Shipping_Management_Application.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shipping_Management_Application.UI
+{
+    internal class UserController
+    {
+        public static User? Login()
+        {
+           
+            using DataContext context = new ();
+           
+            Console.Write("Enter Username:");
+            string? _username = Console.ReadLine();
+            Console.Write("Enter Password:");
+            string? _password = Console.ReadLine();
+
+            CrudOperations.GetUserByUserNameAndPassword(_username, _password);
+
+            return null;
+        }
+
+        public static void RegisterUser()
+        {
+            using DataContext context = new ();
+            Console.Write("Enter Username:");
+            string? _username = Console.ReadLine();
+            Console.Write("Enter Password:");
+            string? _password = Console.ReadLine();
+            User user = new(_username, _password);
+            context.Add(user);
+            context.SaveChanges();
+
+        }
+
+
+    }
+}
