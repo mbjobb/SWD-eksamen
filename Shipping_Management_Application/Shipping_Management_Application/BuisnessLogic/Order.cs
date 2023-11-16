1d6e90adba8f9c90fb54213ed5065a56d04506bf
Henrik-Pham <HenrikPHPham@gmail.com>
2023-11-16 18:20:06 +0100

    pls

--- Shipping_Management_Application/Shipping_Management_Application/BuisnessLogic/Order.cs
+++ Shipping_Management_Application/Shipping_Management_Application/BuisnessLogic/Order.cs
@@ -1,9 +1,4 @@
 ﻿using Shipping_Management_Application.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Shipping_Management_Application.BuisnessLogic
 {
