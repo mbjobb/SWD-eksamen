a2d0afa73a1a3f99c994700183fdc128e047991f
mbJobb <71829138+mbJobb@users.noreply.github.com>
2023-11-25 17:03:43 +0100

    remade CustomerController

--- Shipping_Management_Application/Shipping_Management_Application/BuisnessLogic/Controllers/UserController.cs
+++ Shipping_Management_Application/Shipping_Management_Application/BuisnessLogic/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Shipping_Management_Application.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,12 @@ namespace Shipping_Management_Application.BuisnessLogic.Controllers
 {
     internal class UserController
     {
+        internal Customer CreateCustomer(long id, string? name, string? email, string? address, string? postCode)
+        {
+            Customer customer = new Customer(id, name, email, address, postCode);
+            CrudOperations.CreateCustomer(customer);
+            return customer;
+            
+        }
     }
 }
